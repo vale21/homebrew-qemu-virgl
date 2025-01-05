158bd0990c5aab3f3c45c796bc5b7e9b314b9b83
Vale <vale.v@me.com>
2025-01-05 19:33:33 +0100

    Updating dependencies

--- Formula/virglrenderer.rb
+++ Formula/virglrenderer.rb
@@ -8,8 +8,8 @@ class Virglrenderer < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "knazarov/qemu-virgl/libangle"
-  depends_on "knazarov/qemu-virgl/libepoxy-angle"
+  depends_on "vale21/qemu-virgl/libangle"
+  depends_on "vale21/qemu-virgl/libepoxy-angle"
 
   # waiting for upstreaming of https://github.com/akihikodaki/virglrenderer/tree/macos
   patch :p1 do
